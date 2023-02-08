166c7bc7ff584bf3853be6315f60e5ba15a0d2c6
Daryl V <daryl.villalobos@hotmail.com>
2023-02-08 15:42:11 +0900

    Separate read csv and create summary

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-def comment_pdf(input_file:str, list_filename_csv:str, output_file:str, pages:list=None):
-    import fitz, csv
+def comment_pdf(input_file:str, list_filename_csv:str, pages:list=None):
+    import fitz
+
     comment_title = "Python Highlighter"
     colors = {
         "red": [0.7, 0.35, 0.5],
         "green": [0.35, 0.7, 0.5],
         "blue": [0.35, 0.5, 0.7]
     }
-    """
-    Search for a particular string value in a PDF file and add comments to it.
-    """
-    with open(list_filename_csv, 'r') as csv_data:
-        csv_reader = csv.reader(csv_data)
-        header = next(csv_reader) # skips the first row
-        search_list = [row for row in csv_reader]
-
-    pdfIn = fitz.open(input_file)
-    
+    search_list = read_csv(list_filename_csv)
     # create matches dictionary for output summary
     matches_record = {search[0]: 0 for search in search_list}
-    
+
+    pdfIn = fitz.open(input_file)
     # Iterate throughout the document pages
     for pg,page in enumerate(pdfIn):
         pageID = pg+1
@@ -45,23 +38,31 @@ def comment_pdf(input_file:str, list_filename_csv:str, output_file:str, pages:li
                     annot.set_info(info)
                     annot.update()
     
-    #Save to output file
+    # Save to output file
+    output_file = input_file.split(".")[0] + " comments.pdf"
     pdfIn.save(output_file,garbage=3,deflate=True)
     pdfIn.close()
+    
+    create_summary(input_file, output_file, comment_title, matches_record)
 
-    #Process Summary
+def read_csv(csv_filename):
+    import csv
+    with open(csv_filename, 'r') as csv_data:
+        csv_reader = csv.reader(csv_data)
+        header = next(csv_reader) # skips the first row
+        search_list = [row for row in csv_reader]
+    return search_list
+
+def create_summary(input_file, output_file, comment_title, matches_record):
     summary = {
          "Input File": input_file
-       , "Matching Instances": "\n" + "\n".join("{}: {}".format(word, count) for word, count in matches_record.items())
        , "Output File": output_file
        , "Comment Title": comment_title
+       , "Matching Instances": "\n" + "\n".join("{}: {}".format(word, count) for word, count in matches_record.items())
     }
-
     # Export Process Summary
     with open('summary.txt', 'w') as summary_txt:
         summary_txt.write("\n".join("{}: {}".format(i, j) for i, j in summary.items()))
+    
 
-comment_pdf(input_file="report 2021 EN.pdf"
-            , list_filename_csv="scan_list.csv"
-            , output_file="report 2021 EN comments.pdf"
-            )
+comment_pdf(input_file="report 2021 EN.pdf", list_filename_csv="scan_list.csv")
