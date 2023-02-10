54d39e673ae6263da15f3bc5a72da968a2803fef
Daryl V <daryl.villalobos@hotmail.com>
2023-02-10 16:54:22 +0900

    Update single word scan

--- single_word_scan.py
+++ single_word_scan.py
@@ -1,22 +1,14 @@
-def comment_pdf(input_file:str
-              , search_text:str
-              , comment_title:str
-              , comment_info:str
-              , output_file:str
-              , pages:list=None
-              ):
+def comment_pdf(input_file:str, search_text:str, comment_info:str, pages:list=None):
     import fitz
-    """
-    Search for a particular string value in a PDF file and add comments to it.
-    """
-    pdfIn = fitz.open(input_file)
+    comment_title = "Python Highlighter"
     found_matches = 0
-    # Iterate throughout the document pages
+    pdfIn = fitz.open(input_file)
+    
+    # Iterate throughout pdf pages
     for pg,page in enumerate(pdfIn):
         pageID = pg+1
-        # If required for specific pages
-        if pages:
-           if pageID not in pages:
+        # If required to look in specific pages
+        if pages and pageID not in pages:
               continue
 
         # Use the search_for function to find the text
@@ -39,33 +31,21 @@ def comment_pdf(input_file:str
             annot.update()
 
     #Save to output file
+    output_file = input_file.split(".")[0] + " comments.pdf"
     pdfIn.save(output_file,garbage=3,deflate=True)
     pdfIn.close()
 
     #Process Summary
     summary = {
          "Input File": input_file
-       , "Matching Instances": found_matches
        , "Output File": output_file
        , "Comment Title": comment_title
        , "Comment Info":  comment_info
+       , "Matching Instances": found_matches
     }
 
-    # Print process Summary
-    print("## Summary ########################################################")
-    print("\n".join("{}: {}".format(i, j) for i, j in summary.items()))
-    print("###################################################################")
+    # Export Process Summary
+    with open('summary.txt', 'w') as summary_txt:
+        summary_txt.write("\n".join("{}: {}".format(i, j) for i, j in summary.items()))
 
-# comment_pdf(input_file="report 2021 EN.pdf"
-#             , search_text="human rights"
-#             , comment_title="Python Highlighter"
-#             , comment_info="human rights = 人権"
-#             , output_file="report 2021 EN comments.pdf"
-#             )
-
-comment_pdf(input_file="report 2021 JA.pdf"
-            , search_text="人権"
-            , comment_title="Python Highlighter"
-            , comment_info="human rights = 人権"
-            , output_file="report 2021 JA comments.pdf"
-            )
+comment_pdf(input_file="report 2021 JA.pdf", search_text="人権", comment_info="human rights = 人権")
