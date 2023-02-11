05404b213c9d6d83461720ac9cecbd236a7be9b3
Daryl V <daryl.villalobos@hotmail.com>
2023-02-12 08:59:34 +0900

    Still highlights the same character more than once

--- check_full_width.py
+++ check_full_width.py
@@ -19,32 +19,23 @@ def check_full_width(input_file:str, pages:list=None):
         text = page.get_text("text")
 
         # Split the text by characters and check if each character is full-width
-        full_width_chars = set()
         for char in text:
             status = unicodedata.east_asian_width(char)
             full_status = ['W', 'F', 'A']
             if status in full_status:
-                full_width_chars.add(char)
-                # Update summary
-                if char in results_summary:
-                    results_summary[char][0] += 1
-                else:
-                    results_summary[char] = [1, status]
-
-        # Get the positions of full-width characters in the page
-        for char in full_width_chars:
-            start_idx = 0
-            while True:
-                start_idx = text.find(char, start_idx)
-                if start_idx == -1:
-                    break
+                # Get the positions of full-width characters in the page
+                start_idx = text.find(char)
                 end_idx = start_idx + len(char)
                 matches = page.search_for(text[start_idx:end_idx])
                 if matches:
                     for match in matches:
                         annot = page.add_highlight_annot(match)
                         annot.update()
-                start_idx += 1
+                # Update summary
+                if char in results_summary:
+                    results_summary[char][0] += 1
+                else:
+                    results_summary[char] = [1, status]
 
     print(results_summary)
 
