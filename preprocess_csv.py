fd6ee07e4a53fb5a715e2a495690c2af28b4eec1
Daryl V <daryl.villalobos@hotmail.com>
2024-02-27 16:37:18 +0900

    test complete

--- preprocess_csv.py
+++ preprocess_csv.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('input_folder/AR unchecked check list csv.csv')
+def remove_col():
+    df = pd.read_csv('input_folder/AR unchecked check list csv.csv')
 
-df = df.drop('color', axis=1)
+    df = df.drop('color', axis=1)
 
-print(df.head(10))
+    print(df.head(10))
 
-df.to_csv('input_folder/processed.csv', index=False)
+    df.to_csv('input_folder/processed.csv', index=False)
