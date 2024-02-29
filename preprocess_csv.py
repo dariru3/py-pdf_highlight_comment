f84219f9ae0110220f4f4b955a693c482bf3b38f
Daryl V <daryl.villalobos@hotmail.com>
2024-02-29 10:57:41 +0900

    use config file

--- preprocess_csv.py
+++ preprocess_csv.py
@@ -1,10 +1,9 @@
 import pandas as pd
+from config import config
 
 def remove_col():
-    df = pd.read_csv('input_folder/AR unchecked check list csv.csv')
-
+    df = pd.read_csv(config['keywords_list'])
     df = df.drop('color', axis=1)
-
     print(df.head(10))
 
     df.to_csv('input_folder/processed.csv', index=False)
